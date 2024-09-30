04d97e86b94345302056ee3e1207f7f14692a8fc
Yevhenii Havrylko <yevhenii.havrylko@intel.com>
2024-09-30 17:45:04 -0400

    Update build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-mkdir build
-cd build
-
 export CXXFLAGS="$CXXFLAGS -D__STDC_CONSTANT_MACROS -D__STDC_FORMAT_MACROS -D__STDC_LIMIT_MACROS"
 export LDFLAGS="$LDFLAGS -Wl,--exclude-libs,ALL"
 
 cmake \
+    -DCMAKE_VERBOSE_MAKEFILE=TRUE \
+    -DCMAKE_BUILD_TYPE=Release \
     -DLLVM_DIR=$PREFIX/lib/cmake/llvm \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_PREFIX_PATH=$PREFIX \
     -DLLVMSPIRV_INCLUDED_IN_LLVM=no \
     -DSPIRV_TRANSLATOR_DIR=$PREFIX \
-    -DCOMMON_CLANG_LIBRARY_NAME=opencl_clang \
-    ..
+    -DOPENCL_CLANG_LIBRARY_NAME=opencl_clang \
+    -S . \
+    -B ./build
 
-make -j${CPU_COUNT} VERBOSE=1
-make install
-
-ln -sf $PREFIX/lib/libopencl_clang.so $PREFIX/lib/libcommon_clang.so
+cmake --build ./build
+cmake --build ./build --target install
